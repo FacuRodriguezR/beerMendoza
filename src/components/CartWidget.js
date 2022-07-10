ce4983003af7e81153ba7d7755742883f29203f0
Facundo Rodriguez <facurtprn@gmail.com>
2022-07-10 11:55:45 -0300

    se agrega badge en el carrito de compras

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -2,11 +2,16 @@
 import '../App.css'
 import React from 'react'
 import carrito from '../assets/img/carrito.png'
+import { Badge } from '@mui/material'
+
+
 
 export const CartWidget = () => {
   return (
     <div className='navCarrito'>
-        <img className='carrito' src={carrito}/> <span>0</span>
+        <img className='carrito' src={carrito}/>
+        <Badge badgeContent={4} color="primary">
+        </Badge>
     </div>
   )
 }
