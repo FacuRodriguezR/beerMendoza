9b89b417cb10364c481d10aa80d7919bb129f2ee
Facundo Rodriguez <facurtprn@gmail.com>
2022-08-09 00:26:26 -0300

    orden de compra firebase

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,46 @@
 import React from "react";
-
+import { addDoc, collection, getFirestore } from 'firebase/firestore'
 import { CartContext } from "./CartContext";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 
 const Cart = () => {
-  const {removeItem, clearItems, totalPrice } =useContext(CartContext)
+  const {removeItem, clearItems, totalPrice, cartList } =useContext(CartContext)
   const test = useContext(CartContext);
   console.log(test.cartList);
+
+
+  const order = {
+    buyer:{
+      name: 'Facundo',
+      email: 'facurtprn@gmail.com',
+      phone: '12830123',
+      adress: 'Av Siempre Viva 123'
+    },
+    items: cartList.map(product => ({id: product.id, title: product.name, price: product.price, quantity: product.quantity })),
+    total: totalPrice(),
+  }
+
+
+  const handleClick = () =>{
+    const db = getFirestore();
+    const orderCollection = collection(db, 'orders');
+    addDoc(orderCollection, order)
+    .then (({id}) => alert('Se ha creado la orden Satisfactoriamente. Su id de compra es ' + id))
+    .then (clearItems())
+    .catch (err => alert(err))
+  }
+
+  if (cartList.length === 0) {
+    return(
+      <div className="containerItems">
+        <h1>No hay productos en el carrito 🛒</h1>
+     <Link to="/" className="detalle">Realizar compra</Link>
+        
+      </div>
+    )
+  }
 
   return (
     <>
@@ -31,6 +64,7 @@ const Cart = () => {
     <div className="footerCart">
 
     <button type="button" className="detalle" onClick={() => clearItems()}>Vacíar carrito</button>
+    <button type="button" className="detalle" onClick={handleClick}>Comprar</button>
     </div>
     </>
   );
